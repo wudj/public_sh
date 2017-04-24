6d9b46d13a75384fe235b7b4c551673a4ced6dc7
jimmy <1269731493@qq.com>
2017-04-24 11:29:10 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/wudj/public_sh/master/init.sh)
 # 此文件放在 Java 项目的 web/script 目录下，请在 web/script 目录下执行
 
-cd ../src/main/ \
+cd ./src/main/ \
     && git clone git@git.souche.com:souche-f2e/sad-template.git \
     && mv sad-template antd \
     && rm -rf ./antd/.git \
